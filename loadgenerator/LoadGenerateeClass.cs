8552f206da0dacd3bd4a99c3ba962571f0a1fa9e
jvera-collops <jvera@collateralopportunities.com>
2019-04-16 12:40:36 -0400

    VB-324 method to generate the actual payload

--- loadgenerator/LoadGenerateeClass.cs
+++ loadgenerator/LoadGenerateeClass.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using System.Collections.Concurrent;
 using System.Linq;
 using System.Threading;
+using Newtonsoft.Json.Linq;
+
 
 namespace LoadGeneratorDotnetCore
 {
@@ -34,7 +36,15 @@ namespace LoadGeneratorDotnetCore
 
         public byte[] GeneratePayload()
         {
-            return null;
+            dynamic signal = new JObject();
+            signal.ReceiverId = "1afb3a5a-d4e8-432c-9808-36836a52c12e";
+            signal.BeaconId = "6E64356144A8675DABB6";
+            signal.SignalStrength = 185;
+            signal.TimeStampUtc = ((long)(DateTime.Now - new DateTime(1970, 1, 1)).TotalMilliseconds).ToString();
+            signal.Name = "ReceiverBeaconSignal";
+            signal.Version = "1";
+
+            return Encoding.UTF8.GetBytes(signal.ToString());
         }
 
         public abstract Task GenerateBatchAndSend(int batchSize, bool dryRun, CancellationToken cancellationToken, Func<byte[]> loadGenerator);
